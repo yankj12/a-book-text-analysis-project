1b732ac83c8695e1428c245323123dfc429c7052
yankj12 <yankj12@163.com>
2018-09-29 18:01:50 +0800

    爬取古文书籍信息

--- src/main/java/com/yan/novel/spider/main/GuShiWenMain.java
+++ src/main/java/com/yan/novel/spider/main/GuShiWenMain.java
@@ -1,13 +1,28 @@
 package com.yan.novel.spider.main;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import com.yan.novel.schema.NovelInfo;
+import com.yan.novel.service.facade.NovelInfoDaoService;
+import com.yan.novel.service.impl.NovelInfoDaoServiceSpringImpl;
 import com.yan.novel.spider.service.facade.NovelCrawlService;
+import com.yan.novel.spider.service.impl.AbstrateNovelCrawlServiceImpl;
 import com.yan.novel.spider.service.impl.BookCrawlServiceGuShiWenImpl;
+import com.yan.novel.util.NovelToFilesUtil;
 
 public class GuShiWenMain {
 
 	public static void main(String[] args) {
 		// guwen/book_60.aspx
-		crawlBookFromGuShiWen("guwen/book_60.aspx");
+		//crawlBookFromGuShiWen("guwen/book_60.aspx");
+		// 遍历书籍信息
+		crawlAllBookInfo();
 	}
 
 	
@@ -40,7 +55,88 @@ public class GuShiWenMain {
 	 * 爬取书籍信息
 	 */
 	public static void crawlAllBookInfo(){
-		// https://so.gushiwen.org/guwen/
+		// https://so.gushiwen.org/guwen/default.aspx?p=1
+		AbstrateNovelCrawlServiceImpl novelCrawlService = new BookCrawlServiceGuShiWenImpl();
+		
+		// 书籍信息列表
+		List<NovelInfo> bookInfos = new ArrayList<NovelInfo>();
+		
+		int totalPage = 1;
+		
+		// 页码
+		int pageNo = 1;
+		
+		while(pageNo <= totalPage){
+			String webRootUrl = "https://so.gushiwen.org";
+			String url = webRootUrl + "/guwen/default.aspx?p=" + pageNo;
+			
+			// 请求这个url，获取返回的html内容
+			String content = novelCrawlService.requestUrlByGetMethod(url);
+			
+			
+			// 使用jsoup解析html内容
+			Document document=Jsoup.parse(content);
+			
+			// 获取总页数
+			Element sumPageElement = document.select("label#sumPage").first();
+			totalPage = Integer.parseInt(sumPageElement.text());
+			
+			// 获取书籍信息
+			Element element = document.select("div.main3").first();
+			
+			List<Element> bookElements = element.select("div.left > div.sonspic");
+			
+			if(bookElements != null){
+				for(Element bookElement:bookElements){
+					List<Element> pElements = bookElement.select("div.cont > p");
+					Element novelNameElement = pElements.get(0);
+					Element summaryElement = pElements.get(1);
+					
+					NovelInfo novelInfo = new NovelInfo();
+					
+					// 标题
+					String novelName = novelNameElement.text();
+					System.out.println(pageNo + "/" + totalPage + "," + novelName);
+					
+					novelInfo.setNovelName(novelName);
+					// 简介
+					String novelSummary = summaryElement.text();
+					novelInfo.setNovelSummary(novelSummary);
+					
+					Element bookUrlElement = novelNameElement.select("a").first();
+					String bookUrlToken = bookUrlElement.attr("href");
+					novelInfo.setNovelUrlToken(bookUrlToken);
+					
+					// 处理相对链接
+					String bookUrl = "";
+					String bookRelativeUrl = bookUrlToken;
+					if(bookRelativeUrl != null 
+							&& !"".equals(bookRelativeUrl.trim()) 
+							&& !"#".equals(bookRelativeUrl.trim())){
+						if(bookRelativeUrl.startsWith("/")) {
+							
+						}else {
+							bookRelativeUrl = "/" + bookRelativeUrl;
+						}
+						
+						bookUrl = webRootUrl + bookRelativeUrl;
+					}
+					novelInfo.setNovelUrl(bookUrl);
+					
+					novelInfo.setInsertTime(new Date());
+					novelInfo.setUpdateTime(new Date());
+					
+					bookInfos.add(novelInfo);
+				}
+			}
+			
+			// 页数加1
+			pageNo++;
+		}
+		
+		// 保存书籍信息
+		NovelInfoDaoService novelInfoDaoService = new NovelInfoDaoServiceSpringImpl();
+		novelInfoDaoService.insertBathNovelInfo(bookInfos);
 		
 	}
 
